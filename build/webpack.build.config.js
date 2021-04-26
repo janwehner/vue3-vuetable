e05b540ec5110de74c4dd8e96da50eac15c1dbbe
Jan Wehner <jan.wehner@securepoint.de>
2021-04-26 15:57:26 +0200

    refactor: update configs

--- build/webpack.build.config.js
+++ build/webpack.build.config.js
@@ -2,32 +2,37 @@ var webpack = require("webpack");
 var path = require('path')
 var version = require("./../package.json").version;
 var banner = "/**\n" + " * vuetable-2 v" + version + "\n" + " * https://github.com/ratiw/vuetable-2\n" + " * Released under the MIT License.\n" + " */\n";
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+//var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var StatsPlugin = require("stats-webpack-plugin");
 
 var utils = require('./utils')
 var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 
-var cssFileName = "vuetable-2.css";
-var jsFileName = "vuetable-2.js";
+var cssFileName = "vuetable-3.css";
+var jsFileName = "vuetable-3.js";
 
 if(process.env.MINIFY && process.env.MINIFY === "false"){
-  jsFileName = "vuetable-2-full.js"
+  jsFileName = "vuetable-3-full.js"
 }
 var minifyPlugins = [
   new webpack.LoaderOptionsPlugin({
     minimize: true,
     debug: false
   }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
-    },
-    sourceMap:true,
-    comments: false,
-    beautify: false
+  new TerserPlugin({
+    terserOptions: {
+
+    }
   }),
+  // new webpack.optimize.UglifyJsPlugin({
+  //   compress: {
+  //     warnings: false
+  //   },
+  //   sourceMap:true,
+  //   comments: false,
+  //   beautify: false
+  // }),
 ];
 
 var webpackConfig = merge(baseWebpackConfig, {
@@ -52,7 +57,7 @@ var webpackConfig = merge(baseWebpackConfig, {
       banner: banner,
       raw: true
     }),
-    new ExtractTextPlugin({filename: cssFileName, allChunks: true}),
+   // new ExtractTextPlugin({filename: cssFileName, allChunks: true}),
     new StatsPlugin('stats.json')
   ],
   resolve: {
--- src/index.js
+++ src/index.js
@@ -1,38 +1,26 @@
-import Vuetable from './components/Vuetable.vue'
+import Vuetable from './components/Vuetable.vue';
 
-import VuetableFieldMixin from './components/VuetableFieldMixin.vue'
-import VuetableFieldCheckboxMixin from './components/VuetableFieldCheckboxMixin.vue'
-import VuetablePaginationMixin from './components/VuetablePaginationMixin.vue'
-import VuetablePaginationInfoMixin from './components/VuetablePaginationInfoMixin.vue'
+import VuetableFieldMixin from './components/VuetableFieldMixin.vue';
+import VuetableFieldCheckboxMixin from './components/VuetableFieldCheckboxMixin.vue';
+import VuetablePaginationMixin from './components/VuetablePaginationMixin.vue';
+import VuetablePaginationInfoMixin from './components/VuetablePaginationInfoMixin.vue';
 
-import VuetablePagination from './components/VuetablePagination.vue'
-import VuetablePaginationDropDown from './components/VuetablePaginationDropdown.vue'
-import VuetablePaginationInfo from './components/VuetablePaginationInfo.vue'
-import VuetableFieldCheckbox from './components/VuetableFieldCheckbox.vue'
-import VuetableFieldHandle from './components/VuetableFieldHandle.vue'
-import VuetableFieldSequence from './components/VuetableFieldSequence.vue'
-import VuetableRowHeader from './components/VuetableRowHeader.vue'
-import VuetableColGutter from './components/VuetableColGutter.vue'
-import Promise from 'promise-polyfill'
-import { App } from 'vue';
+import VuetablePagination from './components/VuetablePagination.vue';
+import VuetablePaginationDropDown from './components/VuetablePaginationDropdown.vue';
+import VuetablePaginationInfo from './components/VuetablePaginationInfo.vue';
+import VuetableFieldCheckbox from './components/VuetableFieldCheckbox.vue';
+import VuetableFieldHandle from './components/VuetableFieldHandle.vue';
+import VuetableFieldSequence from './components/VuetableFieldSequence.vue';
+import VuetableRowHeader from './components/VuetableRowHeader.vue';
+import VuetableColGutter from './components/VuetableColGutter.vue';
+//import Promise from 'promise-polyfill';
 
-const rootVariable = (typeof self === 'object' && self.self === self && self) || (typeof global === 'object' && global) || this
-if (!rootVariable.Promise) {
-  rootVariable.Promise = Promise
-}
+// const rootVariable = (typeof self === 'object' && self.self === self && self) || (typeof global === 'object' && global) || this;
+// if (!rootVariable.Promise) {
+//   rootVariable.Promise = Promise;
+// }
 
-const install = (app: App) => {
-  app.component("vuetable", Vuetable);
-  app.component("vuetable-col-gutter", VuetableColGutter)
-  app.component("vuetable-field-checkbox", VuetableFieldCheckbox)
-  app.component("vuetable-field-handle", VuetableFieldHandle)
-  app.component("vuetable-field-sequence", VuetableFieldSequence)
-  app.component("vuetable-pagination", VuetablePagination);
-  app.component("vuetable-pagination-dropdown", VuetablePaginationDropDown);
-  app.component("vuetable-pagination-info", VuetablePaginationInfo);
-  app.component("vuetable-row-header", VuetableRowHeader)
-}
-
+export default Vuetable;
 export {
   Vuetable,
   // Mixins
@@ -49,8 +37,39 @@ export {
   VuetablePaginationDropDown,
   VuetablePaginationInfo,
   VuetableRowHeader,
-
-  install
 };
 
-export default Vuetable;
+// export default {
+//   /**
+//    * @param {import('vue').App} app
+//    */
+//   install: (app) => {
+//     app.component("vuetable", Vuetable);
+//     app.component("vuetable-col-gutter", VuetableColGutter);
+//     app.component("vuetable-field-checkbox", VuetableFieldCheckbox);
+//     app.component("vuetable-field-handle", VuetableFieldHandle);
+//     app.component("vuetable-field-sequence", VuetableFieldSequence);
+//     app.component("vuetable-pagination", VuetablePagination);
+//     app.component("vuetable-pagination-dropdown", VuetablePaginationDropDown);
+//     app.component("vuetable-pagination-info", VuetablePaginationInfo);
+//     app.component("vuetable-row-header", VuetableRowHeader)
+//   },
+// }
+
+// export {
+//   Vuetable,
+//   // Mixins
+//   VuetableFieldMixin,
+//   VuetableFieldCheckboxMixin,
+//   VuetablePaginationInfoMixin,
+//   VuetablePaginationMixin,
+//   // UI components
+//   VuetableColGutter,
+//   VuetableFieldCheckbox,
+//   VuetableFieldHandle,
+//   VuetableFieldSequence,
+//   VuetablePagination,
+//   VuetablePaginationDropDown,
+//   VuetablePaginationInfo,
+//   VuetableRowHeader,
+// };
